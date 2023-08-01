51be167bfc3b08fda02a48c7c3432951c5182e34
spidybhau <138691087+spidybhau@users.noreply.github.com>
2023-08-01 13:32:27 +0530

    forgot

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/spidyynaik/YDNewAuto.git /spidyauto
+  git clone https://github.com/spidybhau/YDTest.git /spidyauto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /spidyauto
